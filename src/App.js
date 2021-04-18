b7eef33c93f13e3772192baeb735dbe7ca375a33
erez.manor <erez.manor@worldmate.com>
2021-04-18 12:24:42 +0300

    add feature

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My new feature B
         </a>
       </header>
     </div>
