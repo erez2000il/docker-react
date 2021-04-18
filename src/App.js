2a47d3ab2cf38954bac0484685031e22277dcffc
erez.manor <erez.manor@worldmate.com>
2021-04-18 12:29:03 +0300

    add feature 2

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>2nd try - feature!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          My new feature B
+          Learn React
         </a>
       </header>
     </div>
